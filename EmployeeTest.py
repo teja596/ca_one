05980bd16ed3c761a32ae7324ba4a3b895bf3cff
teja596 <sp0661@gmail.com>
2021-12-02 01:08:45 +0530

    Adding tests

--- EmployeeTest.py
+++ EmployeeTest.py
@@ -8,5 +8,29 @@ class EmployeeTest(unittest.TestCase):
         pi = e.computePayment(1,'31/10/2021')
         self.assertLessEqual(pi['Net Pay'], pi['Gross Pay'])
 
+    def testOverTimeNotNeg(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(40, '31/10/2021')
+        self.assertFalse(pi['Overtime Pay'] < 0)
+
+    def testRegHourExceed(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(30, '31/10/2021')
+        self.assertLessEqual(pi['Regular Hours Worked'], 30)
+
+    def testHighTaxNotNeg(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(300, '31/10/2021')
+        self.assertFalse(pi['Higher Tax'] < 0)
+
+    def testNetPayNotNeg(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(0, '31/10/2021')
+        self.assertFalse(pi['Net Pay'] < 0)
+
 if __name__ == "__main__":
     EmployeeTest().testNetLessEqualGross()
+    EmployeeTest().testOverTimeNotNeg()
+    EmployeeTest().testRegHourExceed()
+    EmployeeTest().testHighTaxNotNeg()
+    EmployeeTest().testNetPayNotNeg()
